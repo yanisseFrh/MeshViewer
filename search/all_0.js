d5526a7b039285bc8805be80330047e6e3828283
yanisseFrh <100363792+yanisseFrh@users.noreply.github.com>
2026-04-08 21:52:33 +0000

    Deploying to gh-pages from @ yanisseFrh/MeshViewer@c0189f1ddf5c8077c4af6327768cc87174d0cb31 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['camera_0',['Camera',['../classCamera.html',1,'']]],
-  ['clear_1',['clear',['../classMesh.html#a7008e23cbaacd2878b7e81a01ac5d96b',1,'Mesh']]],
-  ['computenormals_2',['computeNormals',['../classMesh.html#aa365461672f50bdcfbc8801d81a13114',1,'Mesh']]]
+  ['centertoorigin_1',['centerToOrigin',['../classMesh.html#abd9f92394e0119f73e95530e50eb0c94',1,'Mesh']]],
+  ['clear_2',['clear',['../classMesh.html#a7008e23cbaacd2878b7e81a01ac5d96b',1,'Mesh']]],
+  ['computenormals_3',['computeNormals',['../classMesh.html#aa365461672f50bdcfbc8801d81a13114',1,'Mesh']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['clear_0',['clear',['../classMesh.html#a7008e23cbaacd2878b7e81a01ac5d96b',1,'Mesh']]],
-  ['computenormals_1',['computeNormals',['../classMesh.html#aa365461672f50bdcfbc8801d81a13114',1,'Mesh']]]
+  ['centertoorigin_0',['centerToOrigin',['../classMesh.html#abd9f92394e0119f73e95530e50eb0c94',1,'Mesh']]],
+  ['clear_1',['clear',['../classMesh.html#a7008e23cbaacd2878b7e81a01ac5d96b',1,'Mesh']]],
+  ['computenormals_2',['computeNormals',['../classMesh.html#aa365461672f50bdcfbc8801d81a13114',1,'Mesh']]]
 ];
